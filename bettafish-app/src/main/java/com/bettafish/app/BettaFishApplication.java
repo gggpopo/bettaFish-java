0685272a7044b362e66fdc5de383039fe1fcd96b
gaomengyang.111 <gaomengyang.111@bytedance.com>
2026-04-09 22:39:15 +0800

    fix: exclude DataSource auto-configuration when no database is configured

--- bettafish-app/src/main/java/com/bettafish/app/BettaFishApplication.java
+++ bettafish-app/src/main/java/com/bettafish/app/BettaFishApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = "com.bettafish")
+@SpringBootApplication(
+    scanBasePackages = "com.bettafish",
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+    }
+)
 @ConfigurationPropertiesScan(basePackages = "com.bettafish.common.config")
 public class BettaFishApplication {
 
